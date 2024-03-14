ab803c250ba7f093c05d53652dcdf441228d4280
csdoge007 <1765796778@qq.com>
2024-03-14 15:32:35 +0800

    修改路由结构

--- appfront/src/router/index.js
+++ appfront/src/router/index.js
@@ -1,15 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Map from '../components/Map.vue';
 import Login from '../views/login/index.vue';
+import Admin from '../views/admin/index.vue';
+import Map from '../components/Map.vue';
 const routes = [
   { path: '/', name: 'login', component: Login },
-  { path: '/select/map', name: 'map', component: Map },
+  { 
+    path: '/Admin/:id', 
+    name: 'admin', 
+    component: Admin,
+    children: [
+      {
+        path: 'map',
+        component: Map,
+        name: 'map',
+      }
+    ] 
+  },
 ]
 
 const router = createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
+  routes, 
 })
 
 export default router;
