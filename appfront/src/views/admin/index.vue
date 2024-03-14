30e32ce7708bcf68a35321c1fdbe4298d0fa8d9f
csdoge007 <1765796778@qq.com>
2024-03-14 15:31:24 +0800

    封装主界面

---
+++ appfront/src/views/admin/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <el-aside>
+      <Navigator></Navigator>
+    </el-aside>
+    <el-container>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+import Navigator from '@/components/Navigator.vue';
+</script>
+
+<style lang="scss" scoped>
+.el-aside {
+  background-color: #242E42;
+  width: 200px;
+}
+.el-main {
+  padding: 0;
+}
+</style>
