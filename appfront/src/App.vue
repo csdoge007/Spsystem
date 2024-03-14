1323add02c955380a0d199d8beebf6bc836d3a6a
csdoge007 <1765796778@qq.com>
2024-03-14 15:34:07 +0800

    重新设置flex

--- appfront/src/App.vue
+++ appfront/src/App.vue
@@ -1,22 +1,8 @@
 <script setup>
-// import Map from './components/Map.vue';
-// import Navigator from './components/Navigator.vue';
-// import Login from './views/login/index.vue';
 </script>
 
 <template>
   <div class="app">
-    <!-- <el-container>
-      <el-aside>
-        <Navigator></Navigator>
-      </el-aside>
-      <el-container>
-        <el-main>
-          <Map></Map>
-        </el-main>
-      </el-container>
-    </el-container> -->
-    <!-- <Login></Login> -->
     <router-view></router-view>
   </div>
 </template>
@@ -24,13 +10,6 @@
 <style scoped>
 .app {
     height: 100%;
-    /* display: flex; */
+    display: flex;
 }
-.el-aside {
-  background-color: #242E42;
-  width: 200px;
-}
-.el-main {
-  padding: 0;
-}
-</style>./views/login/index.vue
+</style>
