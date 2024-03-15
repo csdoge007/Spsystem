f2f5c17486dcae4040a831221332e9cca00bcc4d
csdoge007 <1765796778@qq.com>
2024-03-15 17:56:42 +0800

    设置reponse拦截器实现鉴权功能

--- appfront/src/api/index.js
+++ appfront/src/api/index.js
@@ -1,6 +1,6 @@
 // import Vue from 'vue'
 import Axios from 'axios'
-
+import router from '@/router/index';
 Axios.defaults.timeout = 10000;
 // const baseURL = 'http://localhost:3000/'
 const axiosInstance = Axios.create({
@@ -25,6 +25,21 @@ axiosInstance.interceptors.request.use(config => {
 
 
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    // 对响应数据进行处理
+    return response;
+  },
+  (error) => {
+    
+    // 对响应错误进行处理
+    if (error.response && error.response.status === 401) {
+      // 如果接口返回 401 Unauthorized 错误，说明 token 过期，重定向到登录页面
+      router.push('/');
+    }
+    return Promise.reject(error);
+  }
+);
 
 // 通过拦截器处理csrf问题，这里的正则和匹配下标可能需要根据实际情况小改动
 // axiosInstance.interceptors.request.use((config) => {
