88765103b79bfe54f1037f126c6fcabf417f7173
csdoge007 <1765796778@qq.com>
2024-03-20 21:34:40 +0800

    添加可达性计算接口

--- append/app.js
+++ append/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from './middleware/cors.js';
 import bodyParser from 'body-parser';
-import { getPoi, searchPoi, boxSelectPoi } from './controller/map.js';
+import { getPoi, searchPoi, boxSelectPoi, getAccessibility } from './controller/map.js';
 import authenticateToken from './middleware/jwt.js';
 import { login } from './controller/user.js';
 const app = express();
@@ -25,6 +25,8 @@ app.get('/searchPoi', authenticateToken, searchPoi);
 app.post('/boxSelectPoi', authenticateToken, boxSelectPoi);
 
 app.post('/login', login);
+
+app.get('/getAccessibility', authenticateToken, getAccessibility);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
