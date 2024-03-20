def411c947f9b7f54e70100c9d7527e5b1d64a3b
csdoge007 <1765796778@qq.com>
2024-03-20 17:18:24 +0800

    释放数据库连接

--- append/service/user.js
+++ append/service/user.js
@@ -4,5 +4,12 @@ export async function getUserByAccount(account) {
   const pool_client = await pool.connect();
   const query = "SELECT * FROM users WHERE account = $1";
   const result = await pool_client.query(query, [account]);
+  if (pool_client) {
+    try {
+        pool_client.release(); // 释放数据库连接
+    } catch (err) {
+        console.error('Error releasing pool client:', err);
+    }
+  }
   return result.rows[0];
 }
