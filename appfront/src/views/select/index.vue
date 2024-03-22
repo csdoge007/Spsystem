82fd078a3268c12d07b5b3e751f9b907ba449bc4
csdoge007 <1765796778@qq.com>
2024-03-22 21:31:25 +0800

    封装选址组件

---
+++ appfront/src/views/select/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="select">
+    <LeftSlide class="leftslide" :hideLeft="isHidden" @toggleRotation="toggleRotation"></LeftSlide>
+    <Map></Map>
+  </div>
+</template>
+
+<script setup>
+import Map from '@/components/Map.vue';
+import LeftSlide from '@/components/LeftSlide.vue';
+import { ref } from 'vue';
+const isHidden = ref(true);
+const toggleRotation = () => {
+  isHidden.value = !isHidden.value;
+};
+
+</script>
+
+<style scoped>
+.select {
+  position: relative;
+  display: flex;
+  height: 100%;
+  margin: 0;
+}
+
+</style>
