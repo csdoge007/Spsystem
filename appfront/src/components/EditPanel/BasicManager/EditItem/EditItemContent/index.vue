23a5032b4e97fd4e6f967c7933e7e18bdc7119e5
csdoge007 <1765796778@qq.com>
2024-04-21 21:39:24 +0800

    初步封装edit-item-content

---
+++ appfront/src/components/EditPanel/BasicManager/EditItem/EditItemContent/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="point-items">
+    <div class="point-item" v-for="point in points" :key="point.name">
+      {{point.name}}
+    </div>
+  </div>
+</template>
+ 
+<script setup>
+defineOptions({
+  name: 'EditItemContent',
+});
+const props = defineProps({
+  points: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.point-items {
+  height: 100%;
+}
+.point-item {
+  height: 20px;
+}
+</style>
